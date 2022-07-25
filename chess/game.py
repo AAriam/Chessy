7ca127fd21da0fa5c3284bb5e3ff991d87d3acf9
Armin-Ariamajd <80158628+Armin-Ariamajd@users.noreply.github.com>
2022-07-25 04:50:25 +0200

    add class ChessGame

--- chess/game.py
+++ chess/game.py
@@ -1,13 +1,28 @@
 import numpy as np
 
 
-def new_board() -> np.ndarray:
-    """
-    Create a chessboard in starting position.
-    """
-    board = np.zeros((8, 8), dtype=np.int8)  # Initialize an all-zero 8x8 array
-    board[1, :] = 1  # Set white pawns on row 2
-    board[-2, :] = -1  # Set black pawns on row 7
-    board[0, :] = [4, 2, 3, 5, 6, 3, 2, 4]  # Set white's main pieces on row 1
-    board[-1, :] = -board[0]  # Set black's main pieces on row 8
-    return board
+class ChessGame:
+
+    def __init__(self):
+        # Set instance attributes describing the game state to their initial values
+        self._board: np.ndarray = self.new_board()  # Chessboard in starting position
+        self._turn: int = 1  # Whose turn it is; 1 for white, -1 for black
+        self._can_castle: list = [[1, 1], [1, 1]]  # Castling allowance for white and black
+        self._enpassant: int = -1  # Column where en passant capture is allowed in next move
+        self._fifty_move_draw_count: int = 0  # Count for the fifty move draw rule
+        # Set other useful instance attributes
+        self._game_over: bool = False  # Whether the game is over
+        self._scores: list = [0, 0]  # Score of each player
+        return
+
+    @staticmethod
+    def new_board() -> np.ndarray:
+        """
+        Create a chessboard in starting position.
+        """
+        board = np.zeros((8, 8), dtype=np.int8)  # Initialize an all-zero 8x8 array
+        board[1, :] = 1  # Set white pawns on row 2
+        board[-2, :] = -1  # Set black pawns on row 7
+        board[0, :] = [4, 2, 3, 5, 6, 3, 2, 4]  # Set white's main pieces on row 1
+        board[-1, :] = -board[0]  # Set black's main pieces on row 8
+        return board
