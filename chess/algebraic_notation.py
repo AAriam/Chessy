b20b470e192ed1817903d340dfb831de76963099
Armin-Ariamajd <80158628+Armin-Ariamajd@users.noreply.github.com>
2022-07-29 19:26:13 +0200

    Change piece notation for pawn (p -> P)

--- chess/algebraic_notation.py
+++ chess/algebraic_notation.py
@@ -5,7 +5,7 @@ example = "1.e4 c5 2.Nf3 a6 3.c3 d5 4.exd5 Qxd5 5.d4 Nf6 6.Be2 e6 7.O-O Be7 8.c4
 FILES = {file: idx for idx, file in enumerate("abcdefgh")}  # Columns
 RANKS = {idx: idx - 1 for idx in range(1, 9)}  # Rows
 
-PIECES = {letter: idx + 1 for idx, letter in enumerate("pNBRQK")}
+PIECES = {letter: idx + 1 for idx, letter in enumerate("PNBRQK")}
 PIECES_SYMBOLS_BLACK = {symbol: idx - 6 for idx, symbol in enumerate("♚♛♜♝♞♟")}
 PIECES_SYMBOLS_WHITE = {symbol: idx + 1 for idx, symbol in enumerate("♙♘♗♖♕♔")}
 PIECES_SYMBOLS = PIECES_SYMBOLS_BLACK | PIECES_SYMBOLS_WHITE
